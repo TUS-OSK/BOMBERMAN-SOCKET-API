3ba3822a1e4852bbb39cc2e606e577cbb3b07d2a
Yoshihiro <16bit.gwdkr@gmail.com>
2016-10-07 16:40:45 +0000

    こみっとめっせーじ

--- bomberman.js
+++ bomberman.js
@@ -1,13 +1,12 @@
 var fs = require('fs');
 var http = require('http');
-var socketio = require('socket.io');
+// var socketio = require('socket.io');
 var server = http.createServer();
 
-server.on('request', function(req, res) {
-  // var stream = fs.createReadStream('bomberman.html');
+server.on('request', function(req, res) {            
   res.writeHead(200, {'Content-Type': 'text/html'});
-  // stream.pipe(res);
-  var output = fs.readFile(fs.readFileSync('./bomberman.html', 'utf-8'));
+  var output = fs.readFileSync('./bomberman.html', 'utf-8');
+  // var output = fs.readFile(fs.readFileSync('./bomberman.html', 'utf-8'));    ←誤りの原因
   res.end(output);
 });
 
@@ -18,17 +17,17 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket) {
   // console.log('よくわからんけど入室');
   socket.on('enter', function(data) {
-    io.sockets.emit('enter', data + '入室');
-    console.log(data + '入室');
+    io.sockets.emit('enter', data + ' : 入室');      //入室の情報はどうするか
+    console.log(data + ' : 入室');
   });
   
   socket.on('message', function(data) {
     io.sockets.emit('message', data);
-    console.log('messageを以下送信します' + data);
+    console.log('message : ' + data);
   });
     
   socket.on('disconnect', function(data) {
-    io.sockets.emit('massage', data + '退出');
-    console.log(data + '退出');
+    io.sockets.emit('massage', data + ' : 退出');
+    console.log(data + ' : 退出');
   });
 });
